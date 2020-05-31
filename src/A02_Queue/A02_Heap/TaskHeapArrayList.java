4c09698f12578fcbdd5c8d75c6ce6228117a91dc
Stefan Goisjak-Krammer <s.goisjak-krammer@edu.campus02.at>
2020-05-31 09:37:56 +0200

    A02

--- src/A02_Queue/A02_Heap/TaskHeapArrayList.java
+++ src/A02_Queue/A02_Heap/TaskHeapArrayList.java
@@ -1,8 +1,6 @@
-package A02_Heap;
+package A02_Queue.A02_Heap;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 
 public class TaskHeapArrayList {
 
@@ -24,14 +22,13 @@ public class TaskHeapArrayList {
 	 * @param t Einzufügender Task
 	 */
 	public void insert(Task t) {
-		tasks.add(t);
-
-		Collections.sort(tasks, new Comparator<Task>() {
-				@Override
-				public int compare(Task o1, Task o2) {
-					return Integer.compare(o1.getPriority(),o2.getPriority());
-				}
-		});
+		//add Task to private tasks ArrayList
+		if (tasks.isEmpty()){
+			tasks.add(t);
+		} else {
+			tasks.add(t);
+			siftUp();
+		}
 	}
 
 	/**
@@ -39,9 +36,62 @@ public class TaskHeapArrayList {
 	 * @return Task mit kleinster Priorität
 	 */
 	public Task remove() {
-			if (tasks.isEmpty()){
-				return null;
-			}
-			return tasks.remove(0);
+		//Check if ArrayList is Empty
+		if(tasks.isEmpty()){
+			return null;
+		}
+		Task k = tasks.get(tasks.size() - 1);
+		Task p = tasks.get(0);
+		tasks.set(tasks.size() -1, p);
+		tasks.set(0, k);
+
+		Task temp = tasks.remove(tasks.size()-1);
+		siftDown();
+
+		return temp;
 	}
+
+
+
+	private void siftUp(){
+		int k = tasks.size() - 1;
+
+		while(k>0){
+			int p = (k-1)/2;
+			Task item = tasks.get(k);
+			Task parent = tasks.get(p);
+
+			if(item.getPriority() < parent.getPriority()){
+				//swap
+				tasks.set(k, parent);
+				tasks.set(p, item);
+				k = p;
+			} else {
+				break;
+			}
+		}
+	}
+
+	private void siftDown(){
+		int k = 0;
+		int left = 2*k+1;
+		while (left < tasks.size()){
+			int max = left, right = left+1;
+			if(right < tasks.size()){
+				if(tasks.get(left).getPriority() > tasks.get(right).getPriority()){
+					max++;
+				}
+			}
+			if(tasks.get(k).getPriority() > tasks.get(max).getPriority()){
+				Task temp = tasks.get(k);
+				tasks.set(k, tasks.get(max));
+				tasks.set(max, temp);
+				k=max;
+				left = 2*k+1;
+			}else{
+				break;
+			}
+		}
+	}
+
 }
