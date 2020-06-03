87d8511113ad5ef55e15142dd0ffea51033897ae
D34THW15H <60351566+D34THW15H@users.noreply.github.com>
2020-06-03 21:30:17 +0200

    Implementation of a07 all tests green

--- src/A07_BubbleSort/BubbleSort.java
+++ src/A07_BubbleSort/BubbleSort.java
@@ -7,7 +7,28 @@ public class BubbleSort implements PersonenSort {
 	 * Sortier-Funktion
 	 */
 	public void sort(Person[] personen) {
-
+		if (personen == null)
+		{
+			return;
+		}
+		Person tmpPerson1;
+		for (int i=0; i < personen.length;i++)
+		{
+			for (int j = 0; j<personen.length-i;j++)
+			{
+				if(j+1 == personen.length)
+				{
+					break;
+				}
+				int sortResult = personen[j].compareTo(personen[j+1]);
+				if (sortResult > 0)
+				{
+					tmpPerson1 = personen[j];
+					personen[j] = personen[j+1];
+					personen[j+1] = tmpPerson1;
+				}
+			}
+		}
 	}
 	
 }
--- src/A07_BubbleSort/Person.java
+++ src/A07_BubbleSort/Person.java
@@ -23,7 +23,41 @@ public class Person {
 	 * Vergleicht zwei Personen miteinander
 	 * @return <0, wenn a<b || =0, wenn a=b || >0, wenn a>b
 	 */
+
+
+
 	public int compareTo(Person p) {
-		return 0;
+
+		// überprüfe ob Nachname ident
+		if( nachname == p.getNachname())
+		{
+
+			// überprüfe ob Vorname ident => 0
+			if(vorname == p.getVorname())
+			{
+				return 0;
+			}
+			// wenn vorname von p kleiner vorname => 1
+			else if (vorname.compareTo(p.getVorname()) > 0)
+			{
+				return 1;
+			}
+			// sonst -1 => nichtmehr interessant
+			else
+			{
+				return -1;
+			}
+
+
+		}
+		// Wenn nachname p kleiner nachname => 1
+		else if (nachname.compareTo(p.getNachname()) > 0)
+		{
+			return 1;
+		}
+		// sonst => nicht interessant
+		else {
+			return -1;
+		}
 	}
 }
