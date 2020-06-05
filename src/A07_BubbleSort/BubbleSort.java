2eb7c8d27a6fac9691334ffeada61dca92f0d37a
mario <mario.perner@edu.campus02.at>
2020-06-05 15:43:30 +0200

    BubbleSort optimiert

--- src/A07_BubbleSort/BubbleSort.java
+++ src/A07_BubbleSort/BubbleSort.java
@@ -11,9 +11,11 @@ public class BubbleSort implements PersonenSort {
 		{
 			return;
 		}
+		boolean sorted;
 		Person tmpPerson1;
 		for (int i=0; i < personen.length;i++)
 		{
+			sorted = true;
 			for (int j = 0; j<personen.length-i;j++)
 			{
 				if(j+1 == personen.length)
@@ -23,10 +25,14 @@ public class BubbleSort implements PersonenSort {
 				int sortResult = personen[j].compareTo(personen[j+1]);
 				if (sortResult > 0)
 				{
+					sorted = false;
 					tmpPerson1 = personen[j];
 					personen[j] = personen[j+1];
 					personen[j+1] = tmpPerson1;
 				}
+			}
+			if (sorted){
+				break;
 			}
 		}
 	}
